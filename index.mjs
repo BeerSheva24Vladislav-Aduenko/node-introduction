db6c437c29658e044b8d50c78b1e29517307e426
VladislavAduenko <adyenko.job@gmail.com>
2025-02-03 10:36:02 +0200

    synhronous file system - log data

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import logger from "./logger.mjs";
 import fs from "node:fs";
-const data = fs.readFileSync("index.mjs");
+const data = fs.readFileSync("index.mjs", "utf-8");
+console.log(data);
 
 fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), "utf-8")
