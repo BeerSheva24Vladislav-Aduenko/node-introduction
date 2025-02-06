d45be92f95ec76eec109e4f5b122bf48bb77009c
VladislavAduenko <adyenko.job@gmail.com>
2025-02-06 15:39:24 +0200

    http server introduction

--- index.mjs
+++ index.mjs
@@ -1,7 +1,14 @@
-import stream from "./sender-data.mjs";
-let res = "";
-stream.on("data", (chunk) => res += chunk
-)
-stream.on("end", () => {
-    console.log("no more data");
-})
+import http from "node:http";
+
+const server = http.createServer();
+const port = process.env.PORT ?? 3500;
+server.listen(port);
+console.log(`listening on the port ${port}`);
+server.on("request", async (req, res) => {
+  console.log(req.method, req.url);
+  res.setHeader("Content-Type", "application/json");
+  res.statusCode = 400;
+  res.write("just bad request");
+//   res.write(JSON.stringify({ x: 5 }));
+  res.end();
+});
--- sender-data.mjs
+++
@@ -1,7 +0,0 @@
-import Stream from 'node:stream';
-const stream = new Stream.Readable();
-stream.push('hello');
-stream.push("world");
-stream.push(null);
-
-export default stream;
